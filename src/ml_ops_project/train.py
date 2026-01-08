85001278c4230dadaad4992d16131248d1981286
frejajensen9 <frejaj9@gmail.com>
2026-01-08 13:16:09 +0100

    pep8 added

--- src/ml_ops_project/train.py
+++ src/ml_ops_project/train.py
@@ -1,10 +1,12 @@
 from ml_ops_project.model import Model
 from ml_ops_project.data import MyDataset
 
+
 def train():
     dataset = MyDataset("data/raw")
     model = Model()
     # add rest of your training code here
+
 
 if __name__ == "__main__":
     train()
